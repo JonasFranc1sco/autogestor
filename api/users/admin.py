4c318bf631aa676f404b35fa7a45b2c3398d9f56
JonasFranc1sco <jonasgouveia19@gmail.com>
2026-08-30 22:07:22 -0400

    feat(users): implement custom user admin with creation and change forms

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from users.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta: #type: ignore
+        model = User
+        fields = ("email", "full_name", "role")
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta: #type: ignore
+        model = User
+        fields = ("email", "full_name", "role", "is_active", "is_staff")
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    ordering = ("email",)
+    list_display = ("email", "full_name", "role", "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Informações pessoais"), {"fields": ("full_name", "role")}),
+        (
+            _("Permissões"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "full_name", "role", "password1", "password2"),
+            },
+        ),
+    )
+
+    search_fields = ("email", "full_name")
