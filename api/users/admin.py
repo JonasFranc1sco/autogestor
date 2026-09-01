d6bb4e23172ab0a2fee0942745fec91e6a800bf8
JonasFranc1sco <jonasgouveia19@gmail.com>
2026-09-01 09:59:20 -0800

    feat(admin): implement custom user admin with creation and change forms

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+<<<<<<< HEAD
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
+from users.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta: #type: ignore
+        model = User
+        fields = ("email", "full_name", "role")
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta: #type: ignore
+        model = User
+        fields = ("email", "full_name", "role", "is_active", "is_staff")
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    ordering = ("email",)
+    list_display = ("email", "full_name", "role", "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Informações pessoais"), {"fields": ("full_name", "role")}),
+        (
+            _("Permissões"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "full_name", "role", "password1", "password2"),
+            },
+        ),
+    )
+
+    search_fields = ("email", "full_name")
+=======
+from users.models import User
 # Register your models here.
+>>>>>>> 5eacf8a (fix(serializers): implement create and update methods for Client and Employee serializers)
