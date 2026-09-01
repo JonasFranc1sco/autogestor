71086005b4f3ee605621345e0cec48fe7fdc3a0f
JonasFranc1sco <jonasgouveia19@gmail.com>
2026-09-01 09:53:54 -0800

    fix(serializers): implement create and update methods for Client and Employee serializers

--- api/clients/serializers.py
+++ api/clients/serializers.py
@@ -8,3 +8,28 @@ class ClientSerializer(serializers.ModelSerializer):
         model = Client
         fields = ['id', 'person_type','name', 'phone','email','address','document', 'responsible', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
+        
+    def create(self, validated_data):
+        
+        address_data = validated_data.pop('address')
+        
+        address = Address.objects.create(**address_data)
+        
+        client = Client.objects.create(address=address, **validated_data)
+        
+        return client
+    
+    def update(self, instance, validated_data):
+        address_data = validated_data.pop('address', None)
+        
+        if address_data:
+            address = instance.address
+            for attr, value in address_data.items():
+                setattrs(address, attr, value)
+            address.save()
+            
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        
+        return instance
--- api/employees/serializers.py
+++ api/employees/serializers.py
@@ -3,8 +3,33 @@ from .models import Employee
 from core.serializers import AddressSerializer
 
 class EmployeeSerializer(serializers.ModelSerializer):
-    address = AddressSerializer(read_only=True)
+    address = AddressSerializer()
     class Meta:
         model = Employee
         fields = ['id', 'name', 'phone','email','address','document_cpf', 'document_rg', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
+        
+    def create(self, validated_data):
+        
+        address_data = validated_data.pop('address')
+        
+        address = Address.objects.create(**address_data)
+        
+        employee = Employee.objects.create(address=address, **validated_data)
+        
+        return employee
+    
+    def update(self, instance, validated_data):
+        address_data = validated_data.pop('address', None)
+        
+        if address_data:
+            address = instance.address
+            for attr, value in address_data.items():
+                setattrs(address, attr, value)
+            address.save()
+            
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        
+        return instance
